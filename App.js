252b21696821d475975e95babbd021020d6786c3
saidoud <develop.softoud@gmail.com>
2021-10-31 16:16:30 +0100

    update

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import DrawerNavigator from "./app/navigation/DrawerNavigator";
+import firestore from "@react-native-firebase/firestore";
 
 export default function App() {
+  const usersCollection = async () => {
+    const data = await firestore().collection("Persons").get();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    usersCollection();
+  }, []);
   return (
     <NavigationContainer>
       <DrawerNavigator />
